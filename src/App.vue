e348b77e066d343bab20c307e5b7d763f6daf05c
sxzzz <shenxin7905@gmail.com>
2023-04-25 07:59:30 +1000

    setup

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,32 @@
-<script setup>
-
-
-</script>
 
 <template>
-
-  <div class="">
-  
-    <router-view></router-view>
-   
+  <div>
+    <Header></Header>
+    <h1>testing</h1>
   </div>
- 
+
+  <router-view></router-view>
 
 </template>
+
+<script>
+
+import Header from './components/Header/index.vue';
+export default {
+  components:{
+    Header
+  },
+  data(){
+    return {
+
+    }
+  },
+  methods:{
+
+  },
+}
+
+</script>
 
 <style scoped>
 </style>
