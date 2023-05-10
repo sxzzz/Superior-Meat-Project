d96d85e7b222472e40454a00d6a1da81469b134b
sxzzz <88418205+sxzzz@users.noreply.github.com>
2023-05-10 11:36:25 +1000

    update

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -7,7 +7,7 @@
       <div class="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
         <div class="flex items-center">
           <img class="h-24 w-24 rounded-full ring-4 ring-white sm:h-40 sm:w-40" src="./images/lunaLogo1.jpeg" alt="Luna logo" />
-          <span class="ms-7">My name is Luna</span>
+          <span class="ms-7">My name is Luna testing</span>
         </div>
 
       </div>
