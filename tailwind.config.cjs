43dc5b43c215bacace1ee83608e61fd0b96644f7
sxzzz <88418205+sxzzz@users.noreply.github.com>
2023-05-12 00:12:00 +1000

    Carousel

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,17 +6,16 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
-
-    
-   
+    require("tw-elements/dist/plugin.cjs")
   ],
+  darkMode: "class"
 }
